9690b56ca11188be997aa93874251e932dacd4d4
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2023-10-05 22:44:50 +0800

    update errors

--- internal/domain/errors/errors.go
+++ internal/domain/errors/errors.go
@@ -4,4 +4,22 @@ import "errors"
 
 var (
 	ErrorTimeOut = errors.New("time out error")
+
+	ErrorNoName    = errors.New("no name in input")
+	ErrorNoSurname = errors.New("no surname in input")
+
+	ErrorInvalidName        = errors.New("invalid name")
+	ErrorInvalidSurname     = errors.New("invalid surname")
+	ErrorInvalidPatronymic  = errors.New("invalid patronymic")
+	ErrorInvalidGender      = errors.New("invalid gender")
+	ErrorInvalidAge         = errors.New("invalid age")
+	ErrorInvalidNationality = errors.New("invalid nationality")
+
+	ErrorNothingToChange = errors.New("nothing to change")
+	ErrorNoId            = errors.New("no id")
+	ErrorInvalidId       = errors.New("invalid id")
+
+	ErrorNoSuchUser = errors.New("no such user")
+	ErrorNoSuchUsers = errors.New("no such users")
+	ErrorToAddUser  = errors.New("error to add user")
 )
